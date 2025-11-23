ad73dbde5157250594bf5c8c836f85c49989df9a
Ashlesh Prabhu <ashlesh.prabhu5@gmail.com>
2025-11-23 16:56:40 +0530

    leaderboard page functionality

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -3,9 +3,7 @@ import { db } from "@/db";
 import { user } from "@/db/schema";
 import { desc } from "drizzle-orm";
 
-export async function POST(request: NextRequest) {
-    const { userId } = await request.json();
-
+export async function GET(request: NextRequest) {
     const users = await db
         .select({
             id: user.id,
@@ -15,10 +13,7 @@ export async function POST(request: NextRequest) {
         .from(user)
         .orderBy(desc(user.points));
 
-    const rank = users.findIndex((u) => u.id === userId) + 1;
-
     return NextResponse.json({
         leaderboard: users,
-        myRank: rank,
     });
 }
